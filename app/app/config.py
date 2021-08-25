ca9d4a5c668626ef253d4fe27d2b0728c82668ba
root <root@ip-172-31-47-187.us-west-2.compute.internal>
2021-08-25 11:28:25 +0000

    auto-fix

--- app/app/config.py
+++ app/app/config.py
@@ -6,7 +6,7 @@ from vyper import v
 
 @dataclasses.dataclass
 class PruneConfig:
-    path: str
+    path  : str
     days_to_live: int
 
 
@@ -21,5 +21,7 @@ class Config:
 def get_config() -> Config:
     v.set_config_name("pruner_config")
     v.add_config_path(".")
+
+
     v.read_in_config()
     return Config(prune_configs=v.get("pruner"))
